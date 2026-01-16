d81f69cc2df290040d5e594dc018232a47e6afaa
ShahzaibKhan326 <kshahzaib07@gmail.com>
2026-01-16 13:33:56 -0800

    Understanding Basic of Calculator

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,4 +1,65 @@
 // console.log("hello world")
 
-var keyList = document.querySelector(".clacKeys")
-console.log(keyList)
+var keyList = document.querySelector(".calcKeys")
+var input = document.querySelector("#calcInput")
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let typingSecond = false;
+
+
+keyList.addEventListener("click" , (e)=> 
+{
+    if(e.target.tagName !== "BUTTON") return;
+
+    let key = e.target.innerHTML ;
+
+ if(!isNaN(key))
+ {
+    if(!typingSecond)
+    {
+     firstNumber += key;
+     input.value = firstNumber;
+     console.log("first :" +firstNumber)
+    }
+    else
+    {
+      secondNumber += key;
+      input.value += secondNumber
+      console.log("second :" +secondNumber)
+    }
+ }
+ 
+ else if(key === "+" || key === "-" || key === "x" || key === "÷")
+ {
+     operator = key;
+     typingSecond = true; 
+ }
+
+ else if(key === "=")
+ {
+
+   if(operator === "+")
+   {
+      result =Number(firstNumber)+Number(secondNumber);
+   }
+
+   input.value = result;
+   firstNumber = result.toString();
+   secondNumber = "";
+   operator = "";
+   typingSecond = false;
+
+ }
+
+ else if (key === "Ac") {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        typingSecond = false;
+        input.value = "";
+ }
+   
+    
+})
