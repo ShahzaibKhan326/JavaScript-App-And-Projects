37c92831b307f8f494d043c04b52473ddda700c9
shahzaib khan <kshahzaib07@gmail.com>
2026-01-31 06:35:34 -0800

    Update Card Ui

--- E-commerce-website/app.js
+++ E-commerce-website/app.js
@@ -1,39 +1,39 @@
 
 
-let brands = { 
-    apple: {name: "iphone" ,brand:"apple",color:"black"},
-    samsung: {name: "samsung 41" ,brand:"samsung",color:"silver"},  
-}
+// let brands = { 
+//     apple: {name: "iphone" ,brand:"apple",color:"black"},
+//     samsung: {name: "samsung 41" ,brand:"samsung",color:"silver"},  
+// }
 
-let allBrands = { apple:false , samsung:false }
-
-
-let mySelect = document.querySelector("#brands")
-let card = document.querySelector(".card");
+// let allBrands = { apple:false , samsung:false }
 
 
+// let mySelect = document.querySelector("#brands")
+// let card = document.querySelector(".card");
 
-mySelect.addEventListener("change", function ()
-{
-    let selectBrand = mySelect.value;  
-    let data = brands[selectBrand]
+
+
+// mySelect.addEventListener("change", function ()
+// {
+//     let selectBrand = mySelect.value;  
+//     let data = brands[selectBrand]
    
-    for(let key in allBrands)
-    {
-        allBrands[key]=false;
-    }
+//     for(let key in allBrands)
+//     {
+//         allBrands[key]=false;
+//     }
     
-    if(selectBrand in allBrands)
-    {
-        allBrands[selectBrand] = true;
-    }
+//     if(selectBrand in allBrands)
+//     {
+//         allBrands[selectBrand] = true;
+//     }
     
-    card.innerHTML = 
-    `
-    <h3>${data.name}</h3>
-    `
+//     card.innerHTML = 
+//     `
+//     <h3>${data.name}</h3>
+//     `
 
    
     
-})
+// })
 
